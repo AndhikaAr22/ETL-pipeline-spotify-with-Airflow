2c79748d52a9281ae34bf4668a3c932c77530ca7
Andhika Ardianto <andhika220899@gmail.com>
2024-07-10 16:31:32 +0700

    edit scrip

--- dags/package/package_minio.py
+++ dags/package/package_minio.py
@@ -8,7 +8,7 @@ from airflow.models import Variable
 # class minio
 class MinioBucket:
     def __init__(self):
-        postgres_conn = Connection.get_connection_from_secrets('neon_postgresql')
+        postgres_conn = Connection.get_connection_from_secrets('postgres_id')
         s3_conn = BaseHook.get_connection("spotify_minio")
         extra = s3_conn.extra
         extra_convt = json.loads(extra)
